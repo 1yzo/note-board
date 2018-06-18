b5474199c6bcb876f90c6f8d81c1c745139714c7
Osman Ali <okali@uci.edu>
2018-06-17 18:59:22 -0700

    Another try

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.use(express.static(path.join(__dirname, '..', 'client', 'build', 'static')))
 app.use(bodyParser.json());
 app.use('/api', index);
 
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
 });
 
